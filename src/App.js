0687d8247ab6c9a1f39d3ad3106527797cf6ce81
anadoh <68846949+anadoh@users.noreply.github.com>
2021-08-31 11:54:26 +0200

    users search component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,79 @@
-import React, {Component} from 'react';
-import Navbar from './component/Navbar';
-import Users from './component/users/Users';
-import Search from './component/users/Search';
-import axios from 'axios';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Alert from "./component/Alert";
+import Users from "./component/users/Users";
+import Search from "./component/users/Search";
+import About from "./component/pages/About"
+import axios from "axios";
+import "./App.css";
 
-
-class App extends Component  {
+class App extends Component {
   state = {
     users: [],
-    loading: false
-  }
+    user: {},
+    loading: false,
+    alert: null,
+  };
 
-  async componentDidMount() {
+  searchUsers = async (text) => {
     this.setState({ loading: true });
-  console.log(process.env.REACT_APP_GITHUB_CLIENT_ID)
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    console.log(res);
+    this.setState({ users: res.data.items, loading: false });
+  };
 
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    
-    this.setState({ users: res.data, loading: false});
-   
+
+  getUser = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com//users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ user: res.data, loading: false });
   }
 
+  clearUsers = () => this.setState({ users: [], loading: false });
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
 
   render() {
-     return (
-    <div className="App">
-      <Navbar  />
-      <div>
-        <Search/>
-        <Users loading={this.state.loading} users={this.state.users}/>
-      </div>
-    </div>
-  );
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={this.state.users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      loading={this.state.loading}
+                      users={this.state.users}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About}/>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
   }
-
 }
 
 export default App;
--- src/component/users/Search.js
+++ src/component/users/Search.js
@@ -1,27 +1,56 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { blockStatement } from "@babel/types";
 
 export class Search extends Component {
   state = {
-    text: ''
-  }
+    text: "",
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
+  };
 
   onChange = (e) => this.setState({ text: e.target.value });
   render() {
     return (
-      <form className="form">
-        <input
-          type="text"
-          name="text"
-          placeholder="Search Users..."
-          value={this.state.text}
-          onChange={this.onChange}
-        />
-        <input
-          type="submit"
-          value="Search"
-          className="btn btn-dark btn-block"
-        />
-      </form>
+      <div>
+        <form onSubmit={this.onSubmit} className="form">
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Users..."
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+        {this.props.showClear && (
+          <button
+            className="btn btn-light btn-block"
+            onClick={this.props.clearUsers}
+          >
+            Clear
+          </button>
+        )}
+      </div>
     );
   }
 }
